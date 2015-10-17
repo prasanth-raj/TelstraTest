3d6d45590e61614ba21a8c1ab8e9e1eb80c3ed18
Prasanth Raj S <prasanthrajs@Prasanths-MacBook-Pro.local>
2015-10-17 11:42:26 +0530

    Initial Commit

---
+++ TelstraTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TelstraTestApp
+//
+//  Created by Prasanth Raj S on 17/10/15.
+//  Copyright (c) 2015 Cognizant Technologies Services Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TelstraTestApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TelstraTestApp
+//
+//  Created by Prasanth Raj S on 17/10/15.
+//  Copyright (c) 2015 Cognizant Technologies Services Pvt Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
